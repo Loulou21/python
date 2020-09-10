16494912b751e0c1e864bc9393908daeb58f1e10
louisadam21 <louis.adam67@gmail.com>
2020-09-10 18:23:03 +0200

    fini

--- TD4/Exo1.py
+++ TD4/Exo1.py
@@ -17,14 +17,23 @@ def vol_boite(longueur=None, largeur=None, hauteur=None):
 
 print(vol_boite(4))
 
-def remplacement(c1, c2, ch):
-    toto = ch.split(c1)
-    print(toto)
-    toto.insert(1,c2)
-    return (toto[0]+toto[1]+toto[2])   
-    
+# def remplacement(c1, c2, ch):
+#     toto = ch.split(c1)
+#     print(toto)
+#     toto.insert(1,c2)
+#     return (toto[0]+toto[1]+toto[2])   
+# print(remplacement("bc","toto","abcdefg"))
 
-print(remplacement("bc","toto","abcdefg"))
+def remplacement(c1, c2, ch):
+    ch2 = ""
+    for i in range(len(ch)):
+        if ch[i] == c1:
+            ch2 = ch2 +c2
+        else:
+            ch2 = ch2 + ch[i]
+    return ch2
+print(remplacement("b","toto","abcdbfg"))
+
 
 # def remplacement(c1=" ", c2="*", ch=""):
 #     ch = ch.replace(c1,c2)
--- TD4/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-def remplacement(c1=" ", c2="*", ch=""):
-#     ch = ch.replace(c1,c2)
-#     return ch
-
-# print(remplacement())
